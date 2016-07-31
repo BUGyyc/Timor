8e39106f62cc4e796e3750d7a6e0fd2e5cc20d74
Wzc <wu_zuchang@163.com>
2016-07-31 12:17:20 +0800

    wzc

---
+++ app/src/main/java/com/linkerlab/housemanager/activity/Login_Activity.java
@@ -0,0 +1,43 @@
+package com.linkerlab.housemanager.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.linkerlab.housemanager.R;
+import com.linkerlab.housemanager.base.BaseActivity;
+
+import net.tsz.afinal.annotation.view.ViewInject;
+
+/**
+ * Created by Administrator on 2016/7/30.
+ * wzc
+ */
+public class Login_Activity extends BaseActivity implements View.OnClickListener{
+
+            @ViewInject(id = R.id.job_login)Button mbt_job;
+
+
+    @Override
+    protected void initVariables() {
+
+
+    }
+
+    @Override
+    protected void initViews(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_login);
+        mbt_job.setOnClickListener(this);
+    }
+
+    @Override
+    protected void loadData() {
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
